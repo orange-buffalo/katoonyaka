b521e69a48dc864d23c41129082a1153ab9161a2
orange-buffalo <orange-buffalo@users.noreply.github.com>
2016-01-01 00:45:02 +0100

    improved scrollbar visibility

--- src/main/resources/public/static/js/katoonyaka.scroll.js
+++ src/main/resources/public/static/js/katoonyaka.scroll.js
@@ -2,12 +2,19 @@ var KatoonyakaScroll = function ($rootScope) {
     return {
         restrict: "A",
 
+        scope: {},
+
         link: function (scope, element) {
+            var $window = $(window);
+            scope.scrollBarVisible = false;
+            scope.scrollBarStartPosition = $window.height();
+
             var $frame = $(element);
             $frame.addClass("frame");
             $frame.children().wrapAll("<div class='slidee'/>");
 
             var $scrollBar = $("<div class='scrollbar'><div class='handle'></div></div>");
+            $scrollBar.transition({opacity: 0}, 0);
             $frame.append($scrollBar);
 
             var sly = new Sly($frame,
@@ -22,17 +29,30 @@ var KatoonyakaScroll = function ($rootScope) {
                 })
                 .init();
 
-            var $window = $(window);
+
 
             $window.on("DOMContentLoaded load resize", function () {
+                scope.scrollBarStartPosition = $window.height();
                 sly.reload();
             });
 
-            sly.on("move", function() {
+            sly.on("move", function () {
                 $window.trigger("scroll");
+
+                if (!scope.scrollBarVisible && sly.pos.cur > scope.scrollBarStartPosition) {
+                    $scrollBar.transition({opacity: 1}, 400);
+                    scope.scrollBarVisible = true;
+                }
+
+                if (scope.scrollBarVisible && sly.pos.cur < scope.scrollBarStartPosition) {
+                    $scrollBar.transition({opacity: 0}, 400);
+                    scope.scrollBarVisible = false;
+                }
+
+
             });
 
-            $rootScope.scrollTo = function(pos) {
+            $rootScope.scrollTo = function (pos) {
                 sly.slideTo(pos);
             }
         }
