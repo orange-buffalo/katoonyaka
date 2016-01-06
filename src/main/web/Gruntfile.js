5760a99649d616b2bbe0038eac876bc943a84275
orange-buffalo <orange-buffalo@users.noreply.github.com>
2016-01-06 11:31:04 +0100

    fix for js source map

--- src/main/web/Gruntfile.js
+++ src/main/web/Gruntfile.js
@@ -1,55 +1,59 @@
 module.exports = function (grunt) {
 
-    var saveLicense = require('uglify-save-license');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-compass');
+    var saveLicense = require("uglify-save-license");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-compass");
 
-    // Project configuration.
+    var version = grunt.option("katoonyakaVersion");
+
+    var jsFileNameBase = "dist/js/katoonyaka." + version;
+    var katoonyakaJsFileName = jsFileNameBase + ".js";
+    var katoonyakaMinJsFileName = jsFileNameBase + ".min.js";
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
 
         concat: {
             dist: {
                 src: [
-                    'src/js/third-party/*.js',
-                    'src/js/app/controllers/*.js',
-                    'src/js/app/directives/*.js',
-                    'src/js/app/katoonyaka.js'
+                    "src/js/third-party/*.js",
+                    "src/js/app/controllers/*.js",
+                    "src/js/app/directives/*.js",
+                    "src/js/app/katoonyaka.js"
                 ],
-                dest: 'dist/js/katoonyaka.js'
+                dest: katoonyakaJsFileName
             }
         },
 
         uglify: {
             options: {
                 sourceMap: true,
-                sourceMapIncludeSources: true,
                 preserveComments: saveLicense
             },
             build: {
-                src: 'dist/js/katoonyaka.js',
-                dest: 'dist/js/katoonyaka.min.js'
+                src: katoonyakaJsFileName,
+                dest: katoonyakaMinJsFileName
             }
         },
 
         compass: {
             buildSass: {
                 options: {
-                    basePath: 'src/sass',
-                    config: 'src/sass/.config/config.rb',
-                    cssDir: '../../dist/css',
-                    sassDir: '.',
-                    outputStyle: 'compressed',
+                    basePath: "src/sass",
+                    config: "src/sass/.config/config.rb",
+                    cssDir: "../../dist/css",
+                    sassDir: ".",
+                    outputStyle: "compressed",
                     force: true
                 }
             }
         }
     });
 
-    grunt.registerTask('buildJs', ['concat', 'uglify']);
-    grunt.registerTask('buildCss', ['compass']);
+    grunt.registerTask("buildJs", ["concat", "uglify"]);
+    grunt.registerTask("buildCss", ["compass"]);
 
-    grunt.registerTask('default', ['buildJs', 'buildCss']);
+    grunt.registerTask("default", ["buildJs", "buildCss"]);
 
 };
