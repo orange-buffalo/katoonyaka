7d689adb9cd02a56840d5b55ac4637acf9ba96f1
orange-buffalo <orange-buffalo@users.noreply.github.com>
2016-02-28 11:28:47 +0100

    simplifying the scroll closes gh-38

--- src/main/web/src/js/app/directives/katoonyaka.scroll.js
+++ src/main/web/src/js/app/directives/katoonyaka.scroll.js
@@ -1,69 +1,31 @@
 var KatoonyakaScroll = function ($rootScope, gaService) {
     return {
         restrict: "A",
-
         scope: {},
-
-        link: function (scope, $frame) {
-            scope.scrollBarVisible = false;
+        link: function (scope) {
             scope.firstMove = true;
-
-            $frame.addClass("frame");
-            $frame.children().wrapAll("<div class='slidee'/>");
-
-            var $scrollBar = $("<div class='scrollbar'><div class='handle'></div></div>");
-            $scrollBar.transition({opacity: 0}, 0);
-            $frame.append($scrollBar);
-
-            var sly = new Sly($frame,
-                {
-                    touchDragging: true,
-                    speed: 400,
-                    scrollBar: $scrollBar,
-                    scrollBy: $rootScope.smallScreen? 80 : 100,
-                    dragHandle: true,
-                    dynamicHandle: true,
-                    clickBar: true,
-                    easing: "linear",
-                    releaseSwing: true
-                })
-                .init();
-
-            $rootScope.$on("katoonyaka::layoutChange", sly.reload);
-            $rootScope.$on("katoonyaka::pageTransitionFinished", sly.reload);
+            scope.$body = $("body");
 
             var $scrollButton = $(".scroll-button");
-            $scrollButton.on("click", function() {
+            $scrollButton.on("click", function () {
                 $rootScope.scrollTo($rootScope.viewportHeight);
                 gaService.trackScrollButtonClick();
             });
 
-            sly.on("move", function () {
+            window.onscroll = function () {
                 if (scope.firstMove) {
                     $rootScope.scrollTo($rootScope.viewportHeight);
                     scope.firstMove = false;
-                    $scrollButton.hide(300);
                     gaService.trackFirstScroll();
                 }
-
-                $rootScope.$broadcast("katoonyaka::scroll", sly.pos.cur);
-
-                if (!scope.scrollBarVisible && sly.pos.cur > $rootScope.viewportHeight) {
-                    $scrollBar.transition({opacity: 1}, 400);
-                    scope.scrollBarVisible = true;
+                else {
+                    $rootScope.$broadcast("katoonyaka::scroll", document.body.scrollTop);
                 }
-
-                if (scope.scrollBarVisible && sly.pos.cur < $rootScope.viewportHeight) {
-                    $scrollBar.transition({opacity: 0}, 400);
-                    scope.scrollBarVisible = false;
-                }
-
-            });
-
-            $rootScope.scrollTo = function (pos) {
-                sly.slideTo(pos);
             };
 
+            $rootScope.scrollTo = function (pos) {
+                scope.$body.stop().animate({scrollTop: pos}, "500", "linear");
+            };
         }
     }
 };
